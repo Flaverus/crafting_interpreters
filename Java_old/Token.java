2bce2f51b4688753a298c578f3171825b993e3ed
Florian Schnidrig <florian.schnidrig@coop.ch>
2025-03-07 16:07:04 +0100

    Init project and implement Scanner in Java

---
+++ Java_old/Token.java
@@ -0,0 +1,19 @@
+package com.crafting_interpreters.Java;
+
+class Token {
+    final TokenType type;
+    final String lexeme; // "blob" of characters that represents something
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
